13a3b098f2a0a3abd3e6a9d88546e964cc820f91
Zathiald <A01705403@tec.mx>
2024-05-06 19:03:36 -0600

    Added test cases document

---
+++ Test_cases.py
@@ -0,0 +1,124 @@
+sentence = ["Nantang hahaw"]
+
+# Expected result
+#          S       
+#     _____|____    
+#    NP         VP 
+#    |          |   
+#    N          V  
+#    |          |   
+# nantang     hahaw
+
+sentence = ["Menantang hahaw"]
+
+# Expected result
+
+#            S
+#       _____|______    
+#      NP           VP 
+#   ___|_____       |   
+# Num        N      V  
+#  |         |      |   
+#  me     nantang hahaw
+
+sentence = ["Pxenantang hahaw"]
+
+# Expected result
+
+#            S
+#       _____|______    
+#      NP           VP
+#   ___|_____       |
+# Num        N      V
+#  |         |      |
+# pxe     nantang hahaw
+
+sentence = ["Aynantang hahaw"]
+
+# Expected result
+
+#           S
+#       _____|______
+#      NP           VP
+#   ___|_____       |
+# Num        N      V
+#  |         |      |
+#  ay     nantang hahaw
+
+
+sentence = ["Tute yom"]
+
+# Expected result
+
+#       S
+#   ____|___
+#  NP       VP
+#  |        |
+#  N        V
+#  |        |
+# tute     yom
+
+sentence = ["Ikran taron"]
+
+# Expected result
+
+#        S
+#    ____|____
+#   NP        VP
+#   |         |
+#   N         V
+#   |         |
+# ikran     taron
+
+sentence = ["Tsam kxetse tìng"]
+
+# Expected result
+
+#            S
+#        ____|_____
+#       NP         VP
+#   ____|____      |
+#  N         N     V
+#  |         |     |
+# tsam     kxetse tìng
+
+sentence = ["Atokirina' ean"]
+
+# Expected result
+
+#             S
+#      _______|___
+#     NP          VP
+#     |           |
+#     N           V
+#     |           |
+# atokirina_     ean
+
+sentence = ["Eywa vitrautral tìftia"]
+
+# Expected result
+
+#              S
+#        ______|________
+#       NP              VP
+#   ____|______         |
+#  N           N        V
+#  |           |        |
+# eywa     vitrautral tìftia
+
+sentence = ["Na'vi tìrol"]
+
+# Expected result
+
+#        S
+#    ____|____
+#   NP        VP
+#   |         |
+#   N         V
+#   |         |
+# na_vi     tìrol
+
+
+
+
+
