32e080f0c65e325cf933f718eb1b78b3e8855d87
Zathiald <A01705403@tec.mx>
2024-05-31 23:42:16 -0600

    Apply PEP8 Standard

--- Na'vi_recognizer.py
+++ Na'vi_recognizer.py
@@ -19,15 +19,17 @@ def na_vi_tokenize(sentence):
     return sentence.split()
 
 
-# Define a context-free grammar for Na'vi language
 na_vi_grammar = CFG.fromstring("""
     S -> NP VP
     NP -> N | Num N | N N
     VP -> V
     Num -> 'me' | 'pxe' | 'ay'
-    N -> 'nantang' | 'tute' | 'ikran' | 'tsam' | 'kxetse' | 'atokirina_' | 'eywa' | 'na_vi' | 'kifkey' | 'palulukan' | 'toruk' | 'vitrautral' | 'skxawng' | 'tsmukan' | 'tsmuke'
+    N -> 'nantang' | 'tute' | 'ikran' | 'tsam' | 'kxetse' | 'atokirina_' | 'eywa'
+    N -> 'na_vi' | 'kifkey' | 'palulukan' | 'toruk' | 'vitrautral' | 'skxawng'
+    N -> 'tsmukan' | 'tsmuke'
     V -> 'hahaw' | 'yom' | 'taron' | 'tìng' | 'ean' | 'tìftia' | 'tìrol'
 """)
+
 
 # Create a parser with the defined grammar
 na_vi_parser = nltk.ChartParser(na_vi_grammar)
@@ -44,3 +46,4 @@ for sentence in sentences:
     tokens = na_vi_tokenize(sentence)
     for tree in na_vi_parser.parse(tokens):
         tree.pretty_print()
+
